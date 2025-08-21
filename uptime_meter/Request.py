ba6862b94964068c8f8c5b71bdb8d6fbfb078fcc
SUMMIT\aliag <aliaga.pablo@gmail.com>
2025-08-21 01:29:18 -0300

    Error found! If name contains 2 P lower case letters consistently throws 'Unexpected server error'

--- uptime_meter/Request.py
+++ uptime_meter/Request.py
@@ -1,16 +1,34 @@
 import requests
 from properties import URL_API
 
-def send_request(name: str):
+def send_request(name: str) -> requests.models.Response:
     payload = {'name': name}
     try:
         response = requests.post(URL_API, data=payload)
-        print(f"For '{name}' response:\n\tStatus: {response.status_code}\n\tText: {response.text}")
+        return response
     except requests.RequestException as e:
         print(f"Connection error: {e}")
+
+def send_requests_and_log(name_list: list):
+    for name in name_list:
+        response = send_request(name)
+        print(f"For '{name}' response:\n\tStatus: {response.status_code}\n\tText: {response.text}")
 
 # -------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    send_request(None) # this is expected to fail!
-    send_request("Pablo") # does not fail
+    name_list = []
+    # name_list.append("Pablo")
+    # name_list.append("Antonio")
+    # name_list.append("penelope")
+    # name_list.append("Penelope")
+    # name_list.append("Papa")
+    # name_list.append("papa")
+    # name_list.append("pap")
+    # name_list.append("apap")
+    name_list.append("ap pa")
+    name_list.append("pp")
+    name_list.append("Pp")
+    name_list.append("PP")
+    name_list.append("Penelope Lopez")
+    send_requests_and_log(name_list)
