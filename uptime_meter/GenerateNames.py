936cbb243a62cf56a91b728c7a266c818f12de0a
SUMMIT\aliag <aliaga.pablo@gmail.com>
2025-08-21 16:09:13 -0300

    Adding new set of names to DB + Execution log + Enable/desable a crude way of execution throttling

--- uptime_meter/GenerateNames.py
+++ uptime_meter/GenerateNames.py
@@ -3,6 +3,8 @@ import string
 import sqlite3
 from properties import NAMES_DATABASE_FILE
 
+chars = string.ascii_lowercase + string.ascii_uppercase + "     "
+
 def generate_names(number_of_records: int, name_length: int) -> list:
     names = list()
     for _ in range(number_of_records):
@@ -12,11 +14,7 @@ def generate_names(number_of_records: int, name_length: int) -> list:
     return names
 
 
-def generate_random_string(length: int) -> str:
-    characters = string.ascii_lowercase
-    return generate_random_string(length, characters)
-
-def generate_random_string(length: int, characters: str) -> str:
+def generate_random_string(length: int, characters: str = chars) -> str:
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
 
@@ -48,16 +46,7 @@ def save_names_db(database_file: str, name_list: list, clean_table: bool = True)
 # -------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    name_list = []
-    name_list.append(generate_random_string(10, string.ascii_lowercase))
-    name_list.append(generate_random_string(10, string.ascii_uppercase))
-    name_list.append(None) # Based on Postman tests, this should fail!
-    name_list.append("Johnny Cash")
-    name_list.append("")
-    name_list.append("123456")
-    name_list.append("afsasfasf")
-    name_list.append(generate_random_string(len(string.punctuation), string.punctuation))
-    name_list.append(generate_random_string(260, string.ascii_lowercase + string.whitespace))
+    name_list = generate_names(50, 20)
 
     print(f"Names: {name_list}")
     save_names_db(NAMES_DATABASE_FILE, name_list, True)
--- uptime_meter/Request.py
+++ uptime_meter/Request.py
@@ -1,5 +1,5 @@
 import requests
-from properties import URL_API, REQUEST_LOGS_DATABASE_FILE, CLEAN_LOGS_BEFORE_EXECUTION, NAMES_DATABASE_FILE
+from properties import URL_API, REQUEST_LOGS_DATABASE_FILE, CLEAN_LOGS_BEFORE_EXECUTION, NAMES_DATABASE_FILE, ENABLE_THROTTLING_REQUESTS, MAX_REQUESTS_PER_SECOND
 import sqlite3
 import time
 import random
@@ -22,7 +22,7 @@ def send_requests_and_log(database_file: str, name_list: list, log_in_db: bool =
 
         if CLEAN_LOGS_BEFORE_EXECUTION:
             print("Cleanning logs before execution.")
-            cursor.execute("DELETE FROM request_logs")
+            cursor.execute("DELETE FROM request_logs") # TODO: consider dropping table and recreating it using the schema.sql
 
         for name in name_list:
             response = send_request(name)
@@ -49,6 +49,7 @@ def send_requests_and_log(database_file: str, name_list: list, log_in_db: bool =
 
 def send_requests_and_log_with_timer(database_file: str, name_list: list, duration_in_secods: int, log_in_db: bool = True) -> None:
     try:
+        # TODO: Evaluate openning and closing connection to logs DB outside the flow to reuse connection for reporting
         conn = sqlite3.connect(database_file)
         cursor = conn.cursor()
         print(f"Successfully connected to database: {database_file}")
@@ -56,10 +57,12 @@ def send_requests_and_log_with_timer(database_file: str, name_list: list, durati
         if CLEAN_LOGS_BEFORE_EXECUTION:
             print("Cleanning logs before execution.")
             cursor.execute("DELETE FROM request_logs")
+            cursor.execute("DELETE FROM sqlite_sequence WHERE name='request_logs'") # To restart auto-incremental fields
 
         start_time = time.time()
         print(f"Sending requests for {duration_in_secods} seconds.")
 
+        record_count = 0
         while (time.time() - start_time) < duration_in_secods:
             random_index = random.randint(0, len(name_list) - 1)
             name = name_list[random_index]
@@ -70,12 +73,17 @@ def send_requests_and_log_with_timer(database_file: str, name_list: list, durati
                     "INSERT INTO request_logs (url, name_parameter, response_status, response_text) VALUES (?, ?, ?, ?)",
                     (response.url, name, response.status_code, response.text)
                 )
+                record_count += 1
             
             print(f"For '{name}' response:\tStatus: {response.status_code}\tText: {response.text}")
+            
+            if ENABLE_THROTTLING_REQUESTS: # Enable throttling in properties.py (note this is a crude way to throttle and not an optimize one)
+                time.sleep(1 / MAX_REQUESTS_PER_SECOND)
 
         if log_in_db:
             conn.commit()
-            print(f"Comming {len(name_list)} records.")
+            print(f"Comming {record_count} records.")
+            
         print(f"Finishing execution after {(time.time() - start_time):.2f} seconds.")
 
     except sqlite3.Error as e:
@@ -114,5 +122,5 @@ def get_names_from_db(database_file: str) -> list:
 
 if __name__ == "__main__":
     name_list = get_names_from_db(NAMES_DATABASE_FILE)
-    send_requests_and_log(REQUEST_LOGS_DATABASE_FILE, name_list, False)
-    # send_requests_and_log_with_timer(REQUEST_LOGS_DATABASE_FILE, name_list, 2, False)
+    # send_requests_and_log(REQUEST_LOGS_DATABASE_FILE, name_list, False)
+    send_requests_and_log_with_timer(REQUEST_LOGS_DATABASE_FILE, name_list, 30, True)
